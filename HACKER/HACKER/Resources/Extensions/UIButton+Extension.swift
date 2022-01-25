0105466eac127b14669ca3ec7b8a1e0785d99576
Jisoo Kim <kingwltn@naver.com>
2022-01-25 23:02:51 +0900

    [CHORE] - Extension 추가

---
+++ HACKER/HACKER/Resources/Extensions/UIButton+Extension.swift
@@ -0,0 +1,42 @@
+//
+//  UIButton+Extension.swift
+//  HACKER
+//
+//  Created by 김지수 on 2022/01/25.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    func setInsets(
+        forContentPadding contentPadding: UIEdgeInsets,
+        imageTitlePadding: CGFloat
+    ) {
+        self.contentEdgeInsets = UIEdgeInsets(
+            top: contentPadding.top,
+            left: contentPadding.left,
+            bottom: contentPadding.bottom,
+            right: contentPadding.right + imageTitlePadding
+        )
+        self.titleEdgeInsets = UIEdgeInsets(
+            top: 0,
+            left: imageTitlePadding,
+            bottom: 3,
+            right: -imageTitlePadding
+        )
+    }
+  func setupButton(title: String,
+                   color: UIColor,
+                   font: UIFont,
+                   backgroundColor: UIColor,
+                   state: UIControl.State,
+                   radius: CGFloat) {
+    self.setTitle(title, for: state)
+    self.setTitleColor(color, for: state)
+    self.titleLabel?.font = font
+    self.backgroundColor = backgroundColor
+    self.setRounded(radius: radius)
+  }
+}
+
---
+++ HACKER/HACKER/Resources/Extensions/UIColor+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Extension.swift
+//  HACKER
+//
+//  Created by 김지수 on 2022/01/25.
+//
+
+import Foundation
