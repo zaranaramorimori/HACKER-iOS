295f398f2855f2d08c0ed8aae881a3b997ad22f2
Jisoo Kim <kingwltn@naver.com>
2022-02-16 21:16:43 +0900

    [FEAT] #6 - NickName뷰 레이아웃 구현

--- HACKER/HACKER/Sources/SceneDelegate.swift
+++ HACKER/HACKER/Sources/SceneDelegate.swift
@@ -15,7 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     window = UIWindow(windowScene: windowScene)
-    window?.rootViewController = SignupViewController() // RootViewController를 ViewController에 연결
+    let homeNavigationController = UINavigationController(rootViewController: SignupViewController())
+    homeNavigationController.navigationBar.isHidden = true
+    self.window?.rootViewController = homeNavigationController
     window?.makeKeyAndVisible()
   }
 
