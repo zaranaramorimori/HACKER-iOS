19759deb4c90a625022710261973db3695ec2755
Jisoo Kim <kingwltn@naver.com>
2022-02-20 19:05:23 +0900

    [FEAT] #6 - SplashVC 추가

---
+++ HACKER/HACKER/Sources/ViewControllers/LoginViewController/SplashViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SplashViewController.swift
+//  HACKER
+//
+//  Created by 김지수 on 2022/02/20.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+// MARK: - SplashViewController
+class SplashViewController: UIViewController {
+  
+  // MARK: - Components
+  let logoImageView = UIImageView()
+  
+  // MARK: - LifeCycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .hackerWhite
+    layout()
+  }
+}
+// MARK: - Extensions
+extension SplashViewController {
+  func layout() {
+    layoutLogoImageView()
+  }
+  func layoutLogoImageView() {
+    self.view.add(logoImageView) {
+      $0.image = UIImage(named: "200")
+      $0.snp.makeConstraints {
+        $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(246)
+        $0.centerX.equalToSuperview()
+      }
+    }
+  }
+}
+
