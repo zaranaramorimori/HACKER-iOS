a90aa8b4426e4c4d62cb99e799c10db640d9bfa9
Danna <daeunlee0720@gmail.com>
2022-03-22 01:30:09 +0900

    [CHORE] #11 - 보일러 플레이트 삭제

--- HACKER/HACKERUITests/HACKERUITestsLaunchTests.swift
+++ HACKER/HACKERUITests/HACKERUITestsLaunchTests.swift
@@ -26,7 +26,7 @@ class HACKERUITestsLaunchTests: XCTestCase {
 
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
+        attachment.lifetime = .deleteOnSuccess
         add(attachment)
     }
 }
