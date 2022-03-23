2da33e4771d840283544fa195d9173f5e34daf63
dlwns33 <choi990406@gmail.com>
2022-03-24 02:54:20 +0900

    [FEAT] #13 - 설정 뷰에 navigationBar 추가

--- HACKER/HACKER/Sources/ViewControllers/SettingViewController/SettingViewController.swift
+++ HACKER/HACKER/Sources/ViewControllers/SettingViewController/SettingViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class SettingViewController: UIViewController {
-
+    
+    // MARK: - Components
+    private let navigationBar = HackerNavigationBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configUI()
+        setupAutoLayout()
+    }
+    
+    // MARK: - Custom Method
+    
+    private func configUI() {
+        self.view.backgroundColor = .hackerWhite
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    private func setupAutoLayout() {
+        //    addSubviews([navigationBar])
+        view.add(navigationBar)
+        navigationBar.iconLayout(logoImage: UIImage(named: "settingIcon"), rightImage: nil)
+        navigationBar.popViewController = {
+            self.navigationController?.popViewController(animated: true)
+        }
+        navigationBar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
+            make.height.equalTo(56)
+        }
     }
 }
