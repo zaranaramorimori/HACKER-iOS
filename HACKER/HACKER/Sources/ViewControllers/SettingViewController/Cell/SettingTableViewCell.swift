801f83abeccef159ba680584ea79fcec2e58d00f
dlwns33 <choi990406@gmail.com>
2022-03-25 16:52:32 +0900

    [FEAT] #13 - SettingTableViewCell 생성

--- HACKER/HACKER/Sources/ViewControllers/SettingViewController/Cell/SettingTableViewCell.swift
+++ HACKER/HACKER/Sources/ViewControllers/SettingViewController/Cell/SettingTableViewCell.swift
@@ -11,18 +11,42 @@ import SnapKit
 import Then
 
 class SettingTableViewCell: UITableViewCell {
-
+    
     static let identifier = "SettingTableViewCell"
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    // MARK: - UI
+    private let backgroundImage = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+        // TODO: 이미지 받으면 다운받아서 넣기
+        //        $0.image = UIImage(named: "")
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    private let titleLabel = UILabel().then {
+        $0.font = .subtitleMedium16
     }
-
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Method
+    private func setupLayout() {
+        addSubviews([backgroundImage,
+                     titleLabel])
+        
+        backgroundImage.snp.makeConstraints { make in
+            make.top.bottom.leading.trailing.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(backgroundImage.snp.centerY)
+            make.leading.equalToSuperview().inset(18)
+        }
+    }
 }
