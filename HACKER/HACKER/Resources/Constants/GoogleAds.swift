ed3f3aed898bd5b33492198cc8bc106629131a10
Danna <daeunlee0720@gmail.com>
2022-03-26 03:40:59 +0900

    [ADD] #22 - 파일 세팅

---
+++ HACKER/HACKER/Resources/Constants/GoogleAds.swift
@@ -0,0 +1,14 @@
+//
+//  GoogleAds.swift
+//  HACKER
+//
+//  Created by Danna Lee on 2022/03/26.
+//
+
+import Foundation
+
+struct GoogleAds {
+  static let appID = "ca-app-pub-5655080140655125~6537048644"
+  static let adKey = "ca-app-pub-5655080140655125/8178321422"
+  static let sampleAdKey = "ca-app-pub-3940256099942544/1712485313"
+}
---
+++ HACKER/HACKER/Sources/ViewControllers/RewardAdViewController/RewardAdViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RewardAdViewController.swift
+//  HACKER
+//
+//  Created by Danna Lee on 2022/03/26.
+//
+
+import UIKit
+
+class RewardAdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
