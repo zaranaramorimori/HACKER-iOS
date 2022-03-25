1f05375063d3c7a405ac2889725fb07a0d1c6852
dlwns33 <choi990406@gmail.com>
2022-03-25 19:44:10 +0900

    [FEAT] #13 - SettingTableView 레이아웃 구현

--- HACKER/HACKER/Sources/ViewControllers/SettingViewController/SettingViewController.swift
+++ HACKER/HACKER/Sources/ViewControllers/SettingViewController/SettingViewController.swift
@@ -16,14 +16,17 @@ class SettingViewController: UIViewController {
     private lazy var settingsTableView = UITableView(frame: .zero, style: .grouped).then {
         $0.dataSource = self
         $0.delegate = self
-        $0.estimatedRowHeight = 100
-        $0.contentInset = UIEdgeInsets(top: 44, left: 0, bottom: 0, right: 0)
-//        $0.register(ThumbnailTVC.self, forCellReuseIdentifier: ThumbnailTVC.identifier)
+        $0.backgroundColor = .hackerWhite
+        $0.separatorStyle = .none
+        $0.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.identifier)
         
-        if #available(iOS 15.0, *) {
+        if #available(iOS 15, *) {
             $0.sectionHeaderTopPadding = 0
         }
     }
+    
+    private let settingTitles = ["닉네임 변경", "이용약관", "오픈소스 라이선스",
+                                 "만든 사람들", "로그아웃", "서비스 탈퇴"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +49,13 @@ class SettingViewController: UIViewController {
         }
         navigationBar.snp.makeConstraints { make in
             make.top.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
-            make.height.equalTo(56)
+            make.height.equalTo(72)
+        }
+        settingsTableView.snp.makeConstraints { make in
+            make.top.equalTo(self.navigationBar.snp.bottom).offset(8)
+            make.leading.equalToSuperview().inset(24)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.centerX.equalToSuperview()
         }
     }
 }
@@ -54,35 +63,41 @@ class SettingViewController: UIViewController {
 // MARK: - UITableViewDataSource
 extension SettingViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 6
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: ThumbnailTVC.identifier) as? ThumbnailTVC else { return UITableViewCell() }
-        cell.getCellConfigureAt(indexPath.row)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier) as? SettingTableViewCell else { return UITableViewCell() }
+        
+        cell.selectionStyle = .none
+        cell.titleLabel.text = settingTitles[indexPath.section]
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //TODO: 각 셀을 클릭하면 해당 뷰컨으로 push 해주기
+        print(indexPath.section)
     }
 }
 
 // MARK: - UITableViewDelegate
 extension SettingViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = .clear
+        return headerView
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
+        return UIScreen.main.bounds.size.height * (60/812)
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return UITableView.automaticDimension
-    }
-    
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 0
-    }
-    
-//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        return HomeHeaderView()
-//    }
-    
-    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        return UIView()
     }
 }
