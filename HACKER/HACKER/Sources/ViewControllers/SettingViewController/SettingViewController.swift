ad6e92a2bb965f9b1b2585fc4cfc378b8f8349ec
dlwns33 <choi990406@gmail.com>
2022-03-27 22:06:43 +0900

    [CORRECT] #13 - 설정 에셋 이름 오타 수정

--- HACKER/HACKER/Sources/ViewControllers/SettingViewController/SettingViewController.swift
+++ HACKER/HACKER/Sources/ViewControllers/SettingViewController/SettingViewController.swift
@@ -43,7 +43,7 @@ class SettingViewController: UIViewController {
     
     private func setupAutoLayout() {
         view.addSubviews([navigationBar, settingsTableView])
-        navigationBar.iconLayout(logoImage: UIImage(named: "settingsicon"), rightImage: nil)
+        navigationBar.iconLayout(logoImage: UIImage(named: "settingsIcon"), rightImage: nil)
         navigationBar.popViewController = {
             self.navigationController?.popViewController(animated: true)
         }
