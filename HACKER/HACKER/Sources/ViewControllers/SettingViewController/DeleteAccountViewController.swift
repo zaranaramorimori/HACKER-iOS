79991c7656216685d1440a266755364301e1471d
dlwns33 <choi990406@gmail.com>
2022-03-28 12:27:03 +0900

    [FEAT] #20 - 네비게이션바 생성

--- HACKER/HACKER/Sources/ViewControllers/SettingViewController/DeleteAccountViewController.swift
+++ HACKER/HACKER/Sources/ViewControllers/SettingViewController/DeleteAccountViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class DeleteAccountViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  
+  // MARK: - Components
+  private let navigationBar = HackerNavigationBar()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configUI()
+    setupAutoLayout()
+  }
+  
+  // MARK: - Custom Method
+  
+  private func configUI() {
+    self.view.backgroundColor = .hackerWhite
+    self.navigationController?.navigationBar.isHidden = true
+  }
+  
+  private func setupAutoLayout() {
+    view.addSubviews([navigationBar])
+    navigationBar.iconLayout(logoImage: nil, rightImage: nil)
+    navigationBar.popViewController = {
+      self.navigationController?.popViewController(animated: true)
     }
-
+    navigationBar.snp.makeConstraints { make in
+      make.top.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
+      make.height.equalTo(72)
+    }
+  }
 }
