704325d57beee2647337cd02b9347850a948c86b
dlwns33 <choi990406@gmail.com>
2022-03-28 20:17:56 +0900

    [FEAT] #20 - 우는 청년 추가

--- HACKER/HACKER/Sources/ViewControllers/SettingViewController/DeleteAccountViewController.swift
+++ HACKER/HACKER/Sources/ViewControllers/SettingViewController/DeleteAccountViewController.swift
@@ -17,6 +17,10 @@ class DeleteAccountViewController: UIViewController {
     $0.numberOfLines = 3
   }
   
+  private let cryingManImageView = UIImageView().then {
+    $0.image = UIImage(named: "cryingManImage")
+  }
+  
   private let backButton = UIButton().then {
     $0.setBackgroundImage(UIImage(named: "nextBtnBlack"), for: .normal)
     $0.setupButton(title: "돌아가기", color: .hackerWhite, font: .btnText(ofSize: 32), backgroundColor: .clear, state: .normal, radius: 0)
@@ -29,6 +33,10 @@ class DeleteAccountViewController: UIViewController {
     $0.setUnderline()
     $0.addTarget(self, action: #selector(touchDeleteAccountButton(_:)), for: .touchUpInside)
     $0.isUserInteractionEnabled = true
+  }
+  
+  private let tearDropView = UIView().then {
+    $0.backgroundColor = .hackerBlue
   }
   
   override func viewDidLoad() {
@@ -46,7 +54,8 @@ class DeleteAccountViewController: UIViewController {
   
   private func setupAutoLayout() {
     view.addSubviews([navigationBar, goodbyeLabel,
-                      backButton, deleteAccountButton])
+                      cryingManImageView, tearDropView,
+                      deleteAccountButton, backButton])
     navigationBar.iconLayout(logoImage: nil, rightImage: nil)
     navigationBar.popViewController = {
       self.navigationController?.popViewController(animated: true)
@@ -59,6 +68,22 @@ class DeleteAccountViewController: UIViewController {
       make.top.equalTo(self.navigationBar.snp.bottom).offset(9)
       make.leading.equalToSuperview().inset(24)
     }
+    cryingManImageView.snp.makeConstraints { make in
+      make.centerX.equalToSuperview()
+      make.top.equalTo(goodbyeLabel.snp.bottom).offset(97)
+    }
+    tearDropView.snp.makeConstraints { make in
+      make.leading.trailing.bottom.equalToSuperview()
+      make.height.equalTo(350)
+    }
+    UIView.animate(withDuration: 1.5,
+                   delay: 0.1,
+                   options: .curveEaseInOut,
+                   animations: { () -> Void in
+      self.tearDropView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 350)
+    },
+                   completion: { (didFinish) -> Void in
+    })
     deleteAccountButton.snp.makeConstraints { make in
       make.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(10)
       make.centerX.equalToSuperview()
@@ -68,7 +93,6 @@ class DeleteAccountViewController: UIViewController {
       make.leading.equalToSuperview().inset(24)
       make.centerX.equalToSuperview()
     }
-
   }
   
   // MARK: - @objc
