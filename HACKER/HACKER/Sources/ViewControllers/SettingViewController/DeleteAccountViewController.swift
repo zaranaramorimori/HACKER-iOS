5496c90991fb3d855c63041d84da972d6e07018c
dlwns33 <choi990406@gmail.com>
2022-03-28 12:21:46 +0900

    [FEAT] #20 - 계정삭제 뷰 생성

---
+++ HACKER/HACKER/Sources/ViewControllers/SettingViewController/DeleteAccountViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteAccountViewController.swift
+//  HACKER
+//
+//  Created by Yi Joon Choi on 2022/03/28.
+//
+
+import UIKit
+
+class DeleteAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
