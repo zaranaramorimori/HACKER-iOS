edb4b2c0c31e237ed7fe3b4c598963fcbf88651a
dlwns33 <choi990406@gmail.com>
2022-05-04 01:33:58 +0900

    [CHORE] #31 - token 설정

--- HACKER/HACKER/Resources/Constants/Const.swift
+++ HACKER/HACKER/Resources/Constants/Const.swift
@@ -7,5 +7,5 @@
 
 import Foundation
 struct Const {
-    
+    static let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTksInV1aWQiOiIyMTQ1NzA5MDY3IiwidXNlcm5hbWUiOiJrbWViaW4iLCJuaWNrbmFtZSI6IuuvuOu5iCIsImlhdCI6MTY1MTQ2NDU4OCwiZXhwIjoxNjU0MDU2NTg4LCJpc3MiOiJ6em9uZyJ9.yUcgNhs746-eb_NMbpXODMoLyqJ3ohc9s0_hUorMQwQ"
 }
--- HACKER/HACKER/Resources/Constants/Header.swift
+++ HACKER/HACKER/Resources/Constants/Header.swift
@@ -13,22 +13,39 @@ extension Const {
       ["Content-Type": "application/json"]
     }
     
+//    static func basicHeader() -> [String: String] {
+//      ["Content-Type": "application/json",
+//       "token": UserDefaults.standard.string(forKey: Const.UserDefaultsKey.accessToken) ?? ""]
+//    }
+//
+//    static func tokenHeader() -> [String: String] {
+//      ["token": UserDefaults.standard.string(forKey: Const.UserDefaultsKey.accessToken) ?? ""]
+//    }
+//
+//    static func urlEncodedHeader() -> [String: String] {
+//      ["Content-Type": "application/x-www-form-urlencoded"]
+//    }
+//
+//    static func multipartFormHeader() -> [String: String] {
+//      ["Content-Type": "multipart/form-data",
+//       "token": UserDefaults.standard.string(forKey: Const.UserDefaultsKey.accessToken) ?? ""]
+//    }
     static func basicHeader() -> [String: String] {
       ["Content-Type": "application/json",
-       "token": UserDefaults.standard.string(forKey: Const.UserDefaultsKey.accessToken) ?? ""]
+       "token": Const.accessToken]
     }
     
     static func tokenHeader() -> [String: String] {
-      ["token": UserDefaults.standard.string(forKey: Const.UserDefaultsKey.accessToken) ?? ""]
+      ["token": Const.accessToken]
     }
     
     static func urlEncodedHeader() -> [String: String] {
-      ["Content-Type": "application/x-www-form-urlencoded"]
+      ["Content-Type": Const.accessToken]
     }
     
     static func multipartFormHeader() -> [String: String] {
       ["Content-Type": "multipart/form-data",
-       "token": UserDefaults.standard.string(forKey: Const.UserDefaultsKey.accessToken) ?? ""]
+       "token": Const.accessToken]
     }
   }
 }
