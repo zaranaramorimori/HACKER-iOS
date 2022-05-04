e85aac7075e35be410312b495428721dfd3341c6
dlwns33 <choi990406@gmail.com>
2022-05-04 13:03:23 +0900

    [FEAT] #25 - teamViewController 생성

--- HACKER/HACKER/Sources/ViewControllers/RankingViewController/RankingViewController.swift
+++ HACKER/HACKER/Sources/ViewControllers/RankingViewController/RankingViewController.swift
@@ -155,6 +155,8 @@ extension RankingViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     // TODO: 각 셀을 클릭하면 해당 뷰컨으로 push 해주기
     print(indexPath.section)
+    let nextVC = TeamViewController()
+    navigationController?.pushViewController(nextVC, animated: true)
   }
 }
 
