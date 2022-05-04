a1f1c3ed51565f4aad1991b6e7f0b13b0bf3effb
dlwns33 <choi990406@gmail.com>
2022-05-04 14:15:20 +0900

    [FEAT] #33 - 스크롤 뷰 생성

--- HACKER/HACKER/Sources/ViewControllers/TeamViewController/TeamViewController.swift
+++ HACKER/HACKER/Sources/ViewControllers/TeamViewController/TeamViewController.swift
@@ -20,6 +20,55 @@ class TeamViewController: UIViewController {
     $0.contentMode = .scaleToFill
   }
   
+  private let teamScrollView = UIScrollView().then {
+    $0.backgroundColor = .hackerWhite
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.showsVerticalScrollIndicator = false
+    $0.isScrollEnabled = true
+  }
+  
+  private let teamScrollContainerView = UIView().then {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.contentMode = .scaleToFill
+  }
+  
+  var faceImage = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+    $0.clipsToBounds = true
+    $0.image = UIImage(named: "teamCharacterImage")
+  }
+  
+  var teamIcon = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+    $0.clipsToBounds = true
+    $0.image = UIImage(named: "teamCharacterImage")
+  }
+  
+  var nameLabel = UILabel().then {
+    $0.textColor = .hackerBlack
+    $0.font = .subtitleMedium(ofSize: 16)
+    $0.text = "FILL-IN"
+  }
+  
+  var commitLabel = UILabel().then {
+    $0.textColor = .lightGray
+    $0.font = .subtitleRegular(ofSize: 12)
+    $0.text = "1500 커밋"
+  }
+  
+  private let attackButton = UIButton().then {
+    $0.titleLabel?.font = .btnText(ofSize: 20)
+    $0.setTitle("바로가기", for: .normal)
+    $0.setTitleColor(.hackerBlack, for: .normal)
+    $0.addTarget(self, action: #selector(touchAttackButton(_:)), for: .touchUpInside)
+  }
+  
+  var attackButtonCountLabel = UILabel().then {
+    $0.textColor = .lightGray
+    $0.font = .subtitleRegular(ofSize: 12)
+    $0.text = "X 14"
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     configUI()
@@ -34,7 +83,10 @@ class TeamViewController: UIViewController {
   }
   
   private func setupAutoLayout() {
-    view.addSubviews([navigationBar, dividerLine])
+    view.addSubviews([teamScrollView, navigationBar, dividerLine])
+    teamScrollView.add(teamScrollContainerView)
+    teamScrollContainerView.addSubviews([faceImage, teamIcon, nameLabel,
+                                         commitLabel, attackButton, attackButtonCountLabel])
     navigationBar.iconLayout(isBack: true,
                              logoImage: UIImage(named: "fightMainIcon"),
                              rightImage: UIImage(named: "infoIconBlack"))
@@ -49,6 +101,28 @@ class TeamViewController: UIViewController {
       make.top.equalTo(navigationBar.snp.bottom)
       make.leading.trailing.equalToSuperview()
     }
+    teamScrollView.snp.makeConstraints { make in
+      make.top.equalTo(dividerLine.snp.bottom)
+      make.centerX.leading.trailing.equalToSuperview()
+      make.bottom.equalTo(self.view.safeAreaLayoutGuide)
+    }
+    teamScrollContainerView.snp.makeConstraints { make in
+      make.centerX.top.leading.equalToSuperview()
+      make.bottom.equalTo(self.teamScrollView.snp.bottom)
+      make.width.equalTo(self.view)
+//      make.height.equalTo(1000)
+      make.height.equalTo(self.teamScrollView.snp.height).priority(250)
+    }
+    faceImage.snp.makeConstraints { make in
+      make.top.equalToSuperview().inset(47)
+      make.centerX.equalToSuperview()
+    }
+  }
+  
+  // MARK: - @objc
+  
+  @objc func touchAttackButton(_ sender: UIButton) {
+    print("touchShortCutButton")
   }
   
 }
