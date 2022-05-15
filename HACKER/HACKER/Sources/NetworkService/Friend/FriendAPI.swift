2b4bb080c65b812945f462aea779962bb9165fe7
Danna <daeunlee0720@gmail.com>
2022-05-15 17:25:23 +0900

    [FEAT] #49 - API 코드 작성

---
+++ HACKER/HACKER/Sources/NetworkService/Friend/FriendAPI.swift
@@ -0,0 +1,52 @@
+//
+//  FriendAPI.swift
+//  HACKER
+//
+//  Created by Danna Lee on 2022/05/11.
+//
+
+import Foundation
+import Moya
+
+
+public class FriendAPI {
+  
+  static let shared = FriendAPI()
+  var friendProvider = MoyaProvider<FriendService>(plugins: [MoyaLoggerPlugin()])
+  
+  public init() { }
+  
+  func searchFriendGithub(username: String, completion: @escaping (NetworkResult<Any>) -> Void) {
+    friendProvider.request(.searchFriendGithub(username: username)) { (result) in
+      switch result {
+      case .success(let response):
+        let statusCode = response.statusCode
+        let data = response.data
+        
+        let networkResult = self.judgeGithubStatus(by: statusCode, data)
+        completion(networkResult)
+        
+      case .failure(let err):
+        print(err)
+      }
+    }
+  }
+  
+  private func judgeGithubStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+    let decoder = JSONDecoder()
+    guard let decodedData = try? decoder.decode(GenericResponse<FriendGithubResponse>.self, from: data) else {
+      return .pathErr
+    }
+    
+    switch statusCode {
+    case 200:
+      return .success(decodedData.data ?? "None-Data")
+    case 400..<500:
+      return .requestErr(decodedData.status)
+    case 500:
+      return .serverErr
+    default:
+      return .networkFail
+    }
+  }
+}
