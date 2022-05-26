73f1e1df01e9883ad20fbb2fdfd86a174636364a
dlwns33 <choi990406@gmail.com>
2022-05-26 21:43:41 +0900

    [FEAT] #50 - 닉네임 변경뷰 백버튼 추가

--- HACKER/HACKER/Sources/ViewControllers/SettingViewController/ChangeNicknameViewController.swift
+++ HACKER/HACKER/Sources/ViewControllers/SettingViewController/ChangeNicknameViewController.swift
@@ -10,10 +10,32 @@ import SnapKit
 import Then
 
 class ChangeNicknameViewController: NicknameViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  
+  // MARK: - Components
+  private let popButton = UIButton().then {
+      $0.setBackgroundImage(UIImage(named: "backButtonIcon"), for: .normal)
+      $0.addTarget(self, action: #selector(touchPopButton(_:)), for: .touchUpInside)
+      $0.isUserInteractionEnabled = true
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupAutoLayout()
+  }
+  
+  // MARK: - Custom Method
+  private func setupAutoLayout() {
+    self.view.add(popButton)
+    popButton.snp.makeConstraints { make in
+      make.top.equalTo(self.view.safeAreaLayoutGuide).inset(16)
+      make.leading.equalToSuperview().inset(24)
     }
+  }
+  
+  // MARK: - @objc
+  
+  @objc func touchPopButton(_ sender: UIButton) {
+    self.navigationController?.popViewController(animated: true)
+  }
+  
 }
