6adf8a4ceca35ca80e9fdf827a87814f40d596ec
Danna <daeunlee0720@gmail.com>
2022-06-06 23:09:53 +0900

    [STYLE] #82 - 코드 정리

--- HACKER/HACKER/Resources/Class/LoadingHUD.swift
+++ HACKER/HACKER/Resources/Class/LoadingHUD.swift
@@ -10,19 +10,10 @@ import UIKit
 
 import Lottie
 
-protocol LoadingDelegate {
-  func btnClose()
-}
-
 class LoadingHUD {
   public static let sharedInstance = LoadingHUD()
   
   private var backgroundView: UIView?
-  private var popupView: AnimationView?
-  private var loadingLabel: UILabel?
-  public var btnLoadingClose: UIButton?
-  public var delegate: LoadingDelegate?
-  
   
   class func show() {
     let backgroundView = UIView(frame: CGRect.init(x: 0, y: 0, width: 100, height: 100))
@@ -34,71 +25,25 @@ class LoadingHUD {
       }
       
       $0.animation = Animation.named("lf30_editor_kdpmpt6l")
-      
       $0.contentMode = .scaleAspectFit
-      $0.loopMode = .playOnce
+      $0.loopMode = .loop
       $0.play()
     }
     
     if let window = UIApplication.shared.keyWindow {
       window.addSubview(backgroundView)
-//      window.addSubview(popupView)
-//      window.addSubview(loadingLabel)
-//      window.addSubview(btnClose)
-//
+      
       backgroundView.frame = CGRect(x: 0, y: 0, width: window.frame.maxX, height: window.frame.maxY)
-      backgroundView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
+      backgroundView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
       
-//      popupView.center = window.center
-//      popupView.startAnimating()
-      
-//      loadingLabel.layer.position = CGPoint(x: window.frame.midX, y: popupView.frame.maxY + 5)
-      
-//      btnClose.frame = CGRect(x: 0, y: 0, width: window.frame.maxX, height: 60)
-//      btnClose.layer.position = CGPoint(x: window.frame.midX, y: window.frame.maxY-30)
-      
-      sharedInstance.backgroundView?.removeFromSuperview()
-      sharedInstance.popupView?.removeFromSuperview()
-      sharedInstance.loadingLabel?.removeFromSuperview()
       sharedInstance.backgroundView = backgroundView
-      sharedInstance.popupView = lottieView
-//      sharedInstance.loadingLabel = loadingLabel
-//      sharedInstance.btnLoadingClose = btnClose
+      sharedInstance.backgroundView?.removeFromSuperview()
     }
   }
   
   class func hide() {
-    if let popupView = sharedInstance.popupView,
-       let loadingLabel = sharedInstance.loadingLabel,
-       let btnLoadingClose = sharedInstance.btnLoadingClose,
-       let backgroundView = sharedInstance.backgroundView {
-//      popupView.stopAnimating()
+    if let backgroundView = sharedInstance.backgroundView {
       backgroundView.removeFromSuperview()
-      popupView.removeFromSuperview()
-      loadingLabel.removeFromSuperview()
-      btnLoadingClose.removeFromSuperview()
     }
-  }
-  
-//  class func isAnimation() -> Bool {
-//    return sharedInstance.popupView?.isAnimating ?? false
-//  }
-  
-  class func setHidden() {
-    sharedInstance.backgroundView?.isHidden = true
-    sharedInstance.popupView?.isHidden = true
-    sharedInstance.loadingLabel?.isHidden = true
-  }
-  
-  @objc class func btnCloseLoding () {
-    if sharedInstance.delegate != nil {
-      sharedInstance.delegate?.btnClose()
-    }
-  }
-  
-  private class func getAnimationImageArray() -> [UIImage] {
-    var animationArray: [UIImage] = []
-    animationArray.append(UIImage(named: "HomeWolleyNormal")!)
-    return animationArray
   }
 }
