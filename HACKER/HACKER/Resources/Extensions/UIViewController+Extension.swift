778a97706758c50f63ad94646c1987919d77bd68
dlwns33 <choi990406@gmail.com>
2022-06-13 22:40:48 +0900

    [FEAT[ #99 - 빈공간 터치하면 키보드 내려가는 extension 추가

--- HACKER/HACKER/Resources/Extensions/UIViewController+Extension.swift
+++ HACKER/HACKER/Resources/Extensions/UIViewController+Extension.swift
@@ -165,4 +165,16 @@ extension UIViewController {
       completion()
     })
   }
+  
+  // MARK: 빈 화면을 눌렀을 때 키보드가 내려가도록 처리
+  func dismissKeyboardWhenTappedAround() {
+    let tap: UITapGestureRecognizer =
+    UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+    tap.cancelsTouchesInView = false
+    self.view.addGestureRecognizer(tap)
+  }
+  
+  @objc func dismissKeyboard() {
+    self.view.endEditing(true)
+  }
 }
