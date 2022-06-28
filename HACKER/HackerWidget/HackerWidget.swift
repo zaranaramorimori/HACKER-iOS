df56cc9c21d4053426dbcde7f1b64b8f8eb94037
Danna <danna.wa@kakaobrain.com>
2022-06-28 17:45:54 +0900

    위젯 하다 말았음

--- HACKER/HackerWidget/HackerWidget.swift
+++ HACKER/HackerWidget/HackerWidget.swift
@@ -13,15 +13,15 @@ struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: ConfigurationIntent())
     }
-
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+    
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> Void) {
         let entry = SimpleEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
-
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
@@ -29,7 +29,7 @@ struct Provider: IntentTimelineProvider {
             let entry = SimpleEntry(date: entryDate, configuration: configuration)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -40,18 +40,40 @@ struct SimpleEntry: TimelineEntry {
     let configuration: ConfigurationIntent
 }
 
-struct HackerWidgetEntryView : View {
+struct HackerWidgetEntryView: View {
+    @Environment(\.widgetFamily) private var widgetFamily
     var entry: Provider.Entry
-
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        switch widgetFamily {
+        case .systemSmall, .systemLarge, .systemExtraLarge:
+            ZStack {
+                Text("Small")
+                Text("Large")
+            }
+        case .systemMedium:
+            ZStack {
+                Image(uiImage: UIImage(named: "청년머리") ?? UIImage()).resizable().scaledToFit()
+                AsyncImage(url: URL(string: "https://hacker-image.s3.ap-northeast-2.amazonaws.com/head/2022-06-17/user/69"))
+            }
+        @unknown default:
+            Text("ERROR")
+        }
     }
 }
 
 @main
+struct NSWidgetBundle: WidgetBundle {
+    @WidgetBundleBuilder
+    var body: some Widget {
+        HackerWidget()
+        HackerWidget2()
+    }
+}
+
 struct HackerWidget: Widget {
     let kind: String = "HackerWidget"
-
+    
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             HackerWidgetEntryView(entry: entry)
@@ -61,9 +83,27 @@ struct HackerWidget: Widget {
     }
 }
 
+struct HackerWidget2: Widget {
+    let kind: String = "HackerWidget2"
+    
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+            HackerWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget2")
+        .description("This is an example widget2.")
+    }
+}
+
 struct HackerWidget_Previews: PreviewProvider {
     static var previews: some View {
-        HackerWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+        Group {
+            HackerWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+                .previewContext(WidgetPreviewContext(family: .systemSmall))
+            HackerWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+                .previewContext(WidgetPreviewContext(family: .systemSmall))
+            HackerWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+                .previewContext(WidgetPreviewContext(family: .systemSmall))
+        }
     }
 }
